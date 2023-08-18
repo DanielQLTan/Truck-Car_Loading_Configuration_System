8fe92cd37cb68b663467be3115ac34beb9f9802c
Daniel Tan <100169604+DanielQLTan@users.noreply.github.com>
2023-08-17 21:53:06 -0700

    Add files via upload

--- hardcode.py
+++ hardcode.py
@@ -9,20 +9,23 @@ async def handle_request(websocket):
     car_message = {"生产基地": "上海凯迪", "品牌": "凯迪拉克", "车型": "XT6_2023", "版本": "六座四驱铂金型黑钻版", \
                    "长（MM）": 5056, "宽（MM）": 1964, "高（MM）": 1780, "接近角（°）": 16, "离去角（°）": 21, \
                    "轴距(MM)": 2863, "前轮距(MM)": 1687, "后轮距(MM)": 1683, "轮胎尺寸": "235/55_R20", "扭矩数据(N*m)": 350}
-    supplier_message = {"供应商": "鸿宇轿运", "板车": "A型: 长头7位车", "邮箱": "xxx@yyy.com", "电话": "+86-12345678901"}
-    await websocket.send(json.dumps(truck_message, ensure_ascii=False))
-    await websocket.send(json.dumps(car_message, ensure_ascii=False))
-    await websocket.send(json.dumps(supplier_message, ensure_ascii=False))
+    supplier_message = {"supplier": "鸿宇轿运", "truck_list": "A型: 长头7位车", "email": "xxx@yyy.com", "phone": "+86-12345678901"}
+    async for message in websocket:
+        print(message)
+        # await websocket.send(json.dumps(truck_message, ensure_ascii=False))
+        # await websocket.send(json.dumps(car_message, ensure_ascii=False))
+        await websocket.send(json.dumps(supplier_message, ensure_ascii=False))
+    
 
-    image_path = "img/supp/six_suv.jpg"
-    with open(image_path, 'rb') as image_file:
-        image_data = image_file.read()
-    base64_image = base64.b64encode(image_data).decode('utf-8')
-    await websocket.send(base64_image)
+    # image_path = "img/supp/six_suv.jpg"
+    # with open(image_path, 'rb') as image_file:
+    #     image_data = image_file.read()
+    # base64_image = base64.b64encode(image_data).decode('utf-8')
+    # await websocket.send(base64_image)
 
 
 async def main():
-    async with websockets.serve(handle_request, "localhost", 8001):
+    async with websockets.serve(handle_request, "", 8001):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
--- js/supp.js
+++ js/supp.js
@@ -14,7 +14,7 @@ function get_supp_list() {
 	ws.addEventListener("message", function(e) {
 		var supp_list = JSON.parse(e.data);
 		for (let supp of supp_list) {
-			supp_dd.innerHTML += "<option value=\"" + supp + "\">" + supp + "</option>"
+			supp_dd.innerHTML += "<option value=\"" + supp + "\">" + supp + "</option>";
 		}
 	});
 };
@@ -38,13 +38,14 @@ function get_supp_info() {
 	var supp_e = document.getElementById("email");
 	var supp_p = document.getElementById("phone");
 	var truck_dd = document.getElementById("truck");
+	truck_dd.innerHTML = "<option value=\"\" selected disabled>--请选择装载板车--</option>";
 	ws.addEventListener("message", function(e) {
 		var supp_info = JSON.parse(e.data);
 		supp_e.innerHTML = supp_info.email;
 		supp_p.innerHTML = supp_info.phone;
 		var truck_list = supp_info.truck_list;
 		for (let truck of truck_list) {
-			truck_dd.innerHTML += "<option value=\"" + truck + "\">" + truck + "</option>"
+			truck_dd.innerHTML += "<option value=\"" + truck + "\">" + truck + "</option>";
 		}
 	});
 };
@@ -59,7 +60,7 @@ function get_truck_info() {
 		var request = {
 			"method": "GET",
 			"params": {
-				"key": "truck_info",
+				"key": "supp_truck",
 				"value": {
 					"supplier": supp_choice,
 					"truck": truck_choice
@@ -76,11 +77,11 @@ function get_truck_info() {
 		img.src = "img/supp/placeholder.jpg";
 	}
 	ws.addEventListener("message", function(e) {
-		var truck_info = JSON.parse(e.data);
-		truck_s.innerHTML = truck_info.size;
-		truck_p.src = truck_info.image;
-		for (let i = 0; i < truck_info.angles.length; i++) {
-			angle_list[i].src = truck_info.angles[i]
+		var supp_truck = JSON.parse(e.data);
+		truck_s.innerHTML = supp_truck.size;
+		truck_p.src = supp_truck.image;
+		for (let i = 0; i < supp_truck.angles.length; i++) {
+			angle_list[i].src = supp_truck.angles[i]
 		}
 	});
 };
